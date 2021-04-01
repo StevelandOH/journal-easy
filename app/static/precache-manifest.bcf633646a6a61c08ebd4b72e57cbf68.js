55a389fbafc298e47505c3deba4c8996a5841751
Stephen Szelpal <stephenszelpal@icloud.com>
2021-04-01 16:37:09 -0400

    hopefully the end of it

---
+++ app/static/precache-manifest.bcf633646a6a61c08ebd4b72e57cbf68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc3155e038c2a8c5598726814eb1d588",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30617402394d9f0fd3d7",
+    "url": "/static/css/main.e62d3b9d.chunk.css"
+  },
+  {
+    "revision": "131ab6ca2c1294374cae",
+    "url": "/static/js/2.507f37df.chunk.js"
+  },
+  {
+    "revision": "0962dfb8148f31b7eb9eede1b14ea684",
+    "url": "/static/js/2.507f37df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30617402394d9f0fd3d7",
+    "url": "/static/js/main.c0776ab3.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  },
+  {
+    "revision": "edcf42c90c66b97acf5e116a47e32fda",
+    "url": "/static/media/birds-eye.edcf42c9.jpg"
+  }
+]);
