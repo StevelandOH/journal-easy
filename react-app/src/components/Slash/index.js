88c3f3f3f0bd30cbf4306710e92494318895bf74
Stephen Szelpal <stephenszelpal@icloud.com>
2021-04-01 20:47:13 -0400

    stats styling started

--- react-app/src/components/Slash/index.js
+++ react-app/src/components/Slash/index.js
@@ -1,27 +1,150 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { LineChart } from 'react-chartkick';
+import 'chart.js';
+import './Slash.css';
 
 const Slash = ({ date }) => {
     const sessionUser = useSelector((state) => state.users.user);
+    const ratings = useSelector((state) => state.ratings);
+
     const errors = useSelector((state) =>
         sessionUser ? sessionUser.errors : null
     );
 
-    // const thirtyDays = () => {
-    //     const data = [date];
-    //     Array.from() {
-    //         const prior = new Date(
-    //             new Date().setDate(new Date().getDate() - )
-    //         );
-    //         data.unshift(prior.toLocaleDateString());
-    //     }
-    //     return data;
-    // };
+    const thirtyDays = () => {
+        const data = [date.toLocaleDateString()];
+        Array.from([
+            ,
+            ,
+            ,
+            ,
+            ,
+            ,
+            ,
+            ,
+            ,
+            ,
+            ,
+            ,
+            ,
+            ,
+            ,
+            ,
+            ,
+            ,
+            ,
+            ,
+            ,
+            ,
+            ,
+            ,
+            ,
+            ,
+            ,
+            ,
+            ,
+        ]).map((el, i) => {
+            const prior = new Date(
+                new Date().setDate(new Date().getDate() - i)
+            );
+            data.unshift(prior.toLocaleDateString());
+        });
+        return data;
+    };
+
+    const thirtyRatings = () => {
+        const data = {};
+        const r = Object.values(ratings).reverse();
+
+        thirtyDays()
+            .reverse()
+            .map((el, idx) => {
+                if (r[idx]) {
+                    data[el] = r[idx].rating;
+                } else {
+                    data[el] = null;
+                }
+            });
+        return data;
+    };
+
+    const graphData = thirtyRatings();
 
     if (sessionUser && !errors) {
         return (
-            <div>
-                <h1 style={{ color: 'white' }}>{}</h1>
+            <div className="page-container">
+                <div className="line-chart">
+                    <LineChart
+                        label="RATING"
+                        legend={true}
+                        legend="bottom"
+                        colors={['#a872b8cc', '#000000']}
+                        download={true}
+                        data={graphData}
+                    />
+                </div>
+
+                <div className="date-entry-container">
+                    <div>
+                        <input
+                            placeholder="d"
+                            className="number one"
+                            type="text"
+                        ></input>
+                    </div>
+                    <div>
+                        <input
+                            placeholder="d"
+                            className="number two"
+                            type="text"
+                        ></input>
+                    </div>
+                    <div className="number-slash">/</div>
+                    <div>
+                        <input
+                            placeholder="m"
+                            className="number three"
+                            type="text"
+                        ></input>
+                    </div>
+                    <div>
+                        <input
+                            placeholder="m"
+                            className="number four"
+                            type="text"
+                        ></input>
+                    </div>
+                    <div className="number-slash">/</div>
+                    <div>
+                        <input
+                            placeholder="y"
+                            className="number five"
+                            type="text"
+                        ></input>
+                    </div>
+                    <div>
+                        <input
+                            placeholder="y"
+                            className="number six"
+                            type="text"
+                        ></input>
+                    </div>
+                    <div>
+                        <input
+                            placeholder="y"
+                            className="number seven"
+                            type="text"
+                        ></input>
+                    </div>
+                    <div>
+                        <input
+                            placeholder="y"
+                            className="number eight"
+                            type="text"
+                        ></input>
+                    </div>
+                </div>
             </div>
         );
     } else {
